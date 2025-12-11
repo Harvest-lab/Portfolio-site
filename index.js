84c66e01576b53087126217b50e27f34771bb8b4
Harvest <harvestekoyata@gmail.com>
2025-12-11 15:17:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // for the navbar
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
 
-    // for the navbar
-    fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar").innerHTML = data
+      // for the toggle button
+      const navbarToggle = document.querySelector(".navbar-toggle");
+      const navbarMenu = document.querySelector(".navbar-menu");
 
-        // for the toggle button
-        const navbarToggle = document.querySelector('.navbar-toggle')
-        const navbarMenu = document.querySelector('.navbar-menu')
+      navbarToggle.addEventListener("click", () => {
+        navbarToggle.classList.toggle("active");
+        navbarMenu.classList.toggle("active");
+      });
 
-        navbarToggle.addEventListener("click", () =>{
-            navbarToggle.classList.toggle('active')
-            navbarMenu.classList.toggle('active')
-        })
+      // to highlight the active page
+      const currentPage = window.location.pathname.split("/").pop();
+      const links = document.querySelectorAll(".navbar a");
 
-        // to highlight the active page
-        const currentPage = window.location.pathname.split("/").pop()
-        const links = document.querySelectorAll(".navbar a")
-
-        links.forEach(link => {
-            if(link.getAttribute("href") === currentPage || 
-            (currentPage === "" && link.getAttribute("href") === "index.html")){
-                link.classList.add("active")
-            }
-        })
-
+      links.forEach((link) => {
+        if (
+          link.getAttribute("href") === currentPage ||
+          (currentPage === "" && link.getAttribute("href") === "index.html")
+        ) {
+          link.classList.add("active");
+        }
+      });
     })
-    .catch(error => console.error("Error loading navbar:", error))
+    .catch((error) => console.error("Error loading navbar:", error));
 
-    // for the footer
-    fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data
+  // for the footer
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
     })
-    .catch(error => console.error("Error loading footer:", error))
+    .catch((error) => console.error("Error loading footer:", error));
 
-})
+  const faqs = document.querySelectorAll(".questions-container");
+
+  faqs.forEach((faqs) => {
+    faqs.addEventListener("click", () => {
+      faqs.classList.toggle("active");
+    });
+  });
+});
